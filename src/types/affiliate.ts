07e4336f65dab723d2457288d8081e90c9ac7058
Fabio Weydson <fabioweydson@gmail.com>
2025-11-13 16:08:57 -0300

    feat: afiliate  types added

---
+++ src/types/affiliate.ts
@@ -0,0 +1,10 @@
+export interface Affiliate {
+  id: string;
+  member_id: string;
+  full_name: string;
+  cpf_dni: string;
+  phone: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+}
